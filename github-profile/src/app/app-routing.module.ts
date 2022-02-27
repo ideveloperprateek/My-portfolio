1e719203a4a8e441e77ec04aca99f8ba04061de5
developerPrateek <xyz@abc.com>
2022-02-27 16:33:41 +0530

    feature: Added assests

--- github-profile/src/app/app-routing.module.ts
+++ github-profile/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
+  },
+  {
+    path: '**',
     component: LoginComponent,
   },
 ];
